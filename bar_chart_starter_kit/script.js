59a4ba8b99ee6bc1912f868e94cb0bd2aa8ecd07
magnuslykke <magn965m@stud.kea.dk>
2023-08-30 14:44:28 +0200

    bar chart

--- bar_chart_starter_kit/script.js
+++ bar_chart_starter_kit/script.js
@@ -1,5 +1,24 @@
 const list = document.querySelector("ul");
+const maxColumns = 20;
 
-const li = document.createElement("li");
-li.style.setProperty("--height", "30");
-list.appendChild(li);
+// const li = document.createElement("li");
+// li.style.setProperty("--height", "30");
+// list.appendChild(li);
+
+const valueArray = [];
+setInterval(generateBarCharts, 1000);
+
+function generateBarCharts() {
+  const randomNum = Math.floor(Math.random() * 100 + 1);
+  valueArray.push(randomNum);
+  const li = document.createElement("li");
+  li.style.setProperty("--height", randomNum);
+  list.appendChild(li);
+  if (list.children.length > maxColumns) {
+    list.removeChild(list.firstElementChild);
+  }
+  if (valueArray.length > 5) {
+    valueArray.shift();
+  }
+  console.log("test om det virker", valueArray);
+}
